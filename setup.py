a5cd09402c8e1de2830f87c6e73b147c5a2f2e3f
Pauliina Paasivirta <pauliina.paasivirta@maanmittauslaitos.fi>
2023-02-03 16:08:21 +0200

    feat: use setup.cfg instead of setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="pickLayer",
-    version="0.0.0.dev0",
-    packages=["pickLayer"],
-    package_data={
-        "pickLayer": [
-            "metadata.txt",
-            "*.png",
-            "*.svg",
-            "*.ts",
-            "*.ui",
-        ],
-    },
-    install_requires=[
-        "qgis_plugin_tools>=0.2.0",
-    ],
-)
+setup()
